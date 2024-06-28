12a5994063b389ed6fc421ec28c95a23486b3b0d
Florian Kroiss <florian@kroiss-web.eu>
2024-06-28 11:35:14 +0200

    Call recently added vanilla method to get ore readings

--- src/Server/ProspectingPickPatch.cs
+++ src/Server/ProspectingPickPatch.cs
@@ -1,14 +1,11 @@
 ﻿using HarmonyLib;
 using ProspectTogether.Shared;
-using System;
 using System.Collections.Generic;
 using System.Reflection;
 using Vintagestory.API.Common;
-using Vintagestory.API.Datastructures;
 using Vintagestory.API.MathTools;
 using Vintagestory.API.Server;
 using Vintagestory.GameContent;
-using Vintagestory.ServerMods;
 
 namespace ProspectTogether.Server
 {
@@ -20,58 +17,38 @@ namespace ProspectTogether.Server
             if (world.Side != EnumAppSide.Server)
                 return;
 
-            // Some reflection to get this instance field.
+            // Some reflection to get access to some protected stuff
             ProPickWorkSpace ppws = (ProPickWorkSpace)typeof(ItemProspectingPick).GetField("ppws", BindingFlags.NonPublic |
                      BindingFlags.Instance).GetValue(__instance);
+            MethodInfo GenProbeResultsMethod = typeof(ItemProspectingPick).GetMethod("GenProbeResults", BindingFlags.NonPublic |
+                     BindingFlags.Instance);
 
-            // Code adapted from https://github.com/anegostudios/vssurvivalmod/blob/master/Item/ItemProspectingPick.cs
-            DepositVariant[] deposits = world.Api.ModLoader.GetModSystem<GenDeposits>()?.Deposits;
-            if (deposits == null) return;
-
-            IBlockAccessor blockAccess = world.BlockAccessor;
-            int chunksize = blockAccess.ChunkSize;
-            int regsize = blockAccess.RegionSize;
-
-            IMapRegion reg = world.BlockAccessor.GetMapRegion(pos.X / regsize, pos.Z / regsize);
-            int lx = pos.X % regsize;
-            int lz = pos.Z % regsize;
-
-            pos = pos.Copy();
-            pos.Y = world.BlockAccessor.GetTerrainMapheightAt(pos);
-
-            int[] blockColumn = ppws.GetRockColumn(pos.X, pos.Z);
-
-            var occurences = new List<OreOccurence>();
-
-            foreach (var val in reg.OreMaps)
+            PropickReading results = (PropickReading)GenProbeResultsMethod.Invoke(__instance, new object[] { world, pos });
+            if (results == null)
             {
-                IntDataMap2D map = val.Value;
-                int noiseSize = map.InnerSize;
+                return;
+            }
 
-                float posXInRegionOre = (float)lx / regsize * noiseSize;
-                float posZInRegionOre = (float)lz / regsize * noiseSize;
-
-                int oreDist = map.GetUnpaddedColorLerped(posXInRegionOre, posZInRegionOre);
-
-                if (!ppws.depositsByCode.ContainsKey(val.Key))
+            // Convert results to ProspectTogether format
+            var occurences = new List<OreOccurence>();
+            foreach (var reading in results.OreReadings)
+            {
+                string pageCode = ppws.pageCodes[reading.Key];
+                if (reading.Value.TotalFactor > 0.025)
                 {
-                    continue;
+                    // +2 to offset for our Enum
+                    occurences.Add(new OreOccurence("game:ore-" + reading.Key, pageCode, (RelativeDensity)((int)GameMath.Clamp(reading.Value.TotalFactor * 7.5f, 0, 5) + 2), reading.Value.PartsPerThousand));
                 }
-
-                ppws.depositsByCode[val.Key].GetPropickReading(pos, oreDist, blockColumn, out double ppt, out double totalFactor);
-
-                string pageCode = ppws.pageCodes[val.Key];
-                if (totalFactor > 0.025)
+                else if (reading.Value.TotalFactor > PropickReading.MentionThreshold)
                 {
-                    occurences.Add(new OreOccurence("game:ore-" + val.Key, pageCode, (RelativeDensity)((int)GameMath.Clamp(totalFactor * 7.5f, 0, 5) + 2), ppt));
-                }
-                else if (totalFactor > 0.002)
-                {
-                    occurences.Add(new OreOccurence("game:ore-" + val.Key, pageCode, RelativeDensity.Miniscule, 0));
+                    occurences.Add(new OreOccurence("game:ore-" + reading.Key, pageCode, RelativeDensity.Miniscule, reading.Value.PartsPerThousand));
                 }
             }
 
+            // Send information to Player 
             ProspectTogetherModSystem mod = world.Api.ModLoader.GetModSystem<ProspectTogetherModSystem>();
+            IBlockAccessor blockAccess = world.BlockAccessor;
+            int chunksize = blockAccess.ChunkSize;
             ProspectInfo info = new(new ChunkCoordinate(pos.X / chunksize, pos.Z / chunksize), occurences);
             mod.ServerStorage.UserProspected(info, splr);
         }
